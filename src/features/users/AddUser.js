b3bbb4c2abdd0aa023efa1baaa36a2fd9750c8ff
Dimitar <wopz1337@gmail.com>
2022-09-24 16:12:06 +0300

    Get Value & Add In State

--- src/features/users/AddUser.js
+++ src/features/users/AddUser.js
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import AddUserButton from "../../components/AddUserButton";
 import TextField from "../../components/TextField";
 
 const AddUser = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleAddUser = () => {};
   return (
     <div className="mt-10 max-w-xl mx-auto">
       <TextField
         label="Name"
+        value={values.name}
+        onChange={(e) => setValues({ ...values, name: e.target.value })}
         inputProps={{ type: "text", placeholder: "Jhon Doe" }}
       />
       <br />
       <TextField
         label="Email"
+        value={values.email}
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
         inputProps={{ type: "email", placeholder: "jhondoe@mail.com" }}
       />
-      <AddUserButton>Submit</AddUserButton>
+      <AddUserButton onClick={handleAddUser}>Submit</AddUserButton>
     </div>
   );
 };
