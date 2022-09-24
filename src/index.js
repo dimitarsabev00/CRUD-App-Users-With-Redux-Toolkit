d1afaeab862b7a25a28eee986657f84f1b457354
Dimitar <wopz1337@gmail.com>
2022-09-24 15:54:56 +0300

    Added BrowserRouter From React Router DOM

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root");
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
