2e1275a703afafecc314c6d015768fafa9db4ecf
Dimitar <wopz1337@gmail.com>
2022-09-24 16:42:10 +0300

    Provide Redux Store In Applicaton

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Provider } from "react";
+import store from "./features/store/store";
 const container = document.getElementById("root");
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
