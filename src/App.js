a0bbf9c947639c1682c799e2817a9aa0c7d62980
Dimitar Sabev <wopz1337@gmail.com>
2022-10-03 08:36:36 -0100

    Update Title In App Component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
       <h1 className="text-center font-bold text-2xl text-gray-700">
-        UI-Add-Users-In-Collection-With-Redux-Toolkit
+        CRUD-App-Users-With-Redux-Toolkit
       </h1>
       <Routes>
         <Route path="/" element={<UserList />} />
